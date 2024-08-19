db8ad95fc6b225764f4274692211518aadff43d2
expen6ve <christianricosalvana@gmail.com>
2024-08-19 10:39:25 +0800

    fixed image display functionality

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-// Select all elements with the class 'card'
 const card = document.querySelectorAll('.card');
-
-// Loop through each card
 card.forEach(card => {
-    // Add a click event listener to each card
     card.addEventListener('click', () => {
-        // Get the URL from the 'data-url' attribute
         const url = card.getAttribute('data-url');
-        // Redirect the user to the specified URL
         window.location.href = url;
     });
 });
+const food = document.querySelectorAll('.food');
+food.forEach(food => {
+    const url = food.getAttribute('data-img-url');
+    food.style.backgroundImage = `url(${url})`;
+    food.style.backgroundSize = 'cover'; 
+    food.style.backgroundRepeat = 'no-repeat'; 
+});
+
